7cde9a08c3238e4928bed419c9f67c55abe9c63e
Georgios Nordqvist <geonor@kth.se>
2024-10-22 11:13:29 +0200

    Removed code skeleton from .py file

--- Currency_Converter.py
+++ Currency_Converter.py
@@ -5,15 +5,8 @@ print(msft.info)
 
 class CurrencyConverter:
     "Class to convert amounts in one currency (currency1) into the equivalent amount in another currency currency2. "
-    def __init__(self, amount, currency1, currency2):
-        """Initiates the class instance with the following attributes:
-        self.currency1 - the currency to be converted FROM
-        self.currency2 - the currency to be converted TO
-        self.amount =  the amount of currency1 that will be converted into currency2 """
-        self.amount = amount
-        self.currency1 = currency1
-        self.currency2 = currency2
-        self.Xrate = None # The yfinance call to the 'Ticker' of the exchange rate will be made here.
+    def __init__(self):
+        pass
 
     "Method to perform the conversion. Returns the amount in currency 2 that the amount of currency 1 is equivalent to."
     def convert(self):
